f6abbcbaed1869af2b2ca554e34d0fab1c917e9d
juan felipe <riverafelipe3312@gmail.com>
2025-05-02 16:23:35 -0500

    Subiendo carpeta app_crud_ok

---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,146 @@
+import 'package:app_dividido_registro/screens/delete_user_screen.dart';
+import 'package:app_dividido_registro/screens/register_screen.dart';
+import 'package:app_dividido_registro/screens/update_user_screen.dart';
+import 'package:app_dividido_registro/screens/user_list_screen.dart';
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  Future<void> _login() async {
+    final String email = _emailController.text;
+    final String password = _passwordController.text;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final response = await ApiService.login(email, password);
+
+      if (response['statusCode'] == 200) {
+        _showSuccessDialog(context, 'Bienvenido: ${response['user']['email']}');
+      } else {
+        final errorDetail = response['detail'];
+
+        String errorMessage;
+        if (errorDetail is List) {
+          errorMessage = errorDetail.join(', ');
+        } else if (errorDetail is String) {
+          errorMessage = errorDetail;
+        } else {
+          errorMessage = 'Ocurrió un error desconocido.';
+        }
+
+        _showErrorDialog(context, errorMessage);
+      }
+    } catch (e) {
+      _showErrorDialog(context, 'No se pudo conectar al servidor. Error: $e');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _showSuccessDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Login exitoso'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: _emailController,
+          decoration: InputDecoration(labelText: 'Email'),
+        ),
+        TextField(
+          controller: _passwordController,
+          decoration: InputDecoration(labelText: 'Password'),
+          obscureText: true,
+        ),
+        SizedBox(height: 20),
+        _isLoading
+            ? CircularProgressIndicator()
+            : ElevatedButton(onPressed: _login, child: Text('Login')),
+        SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegisterScreen()),
+            );
+          },
+          child: Text('¿No tienes cuenta? Regístrate'),
+        ),
+        SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DeleteUserScreen()),
+            );
+          },
+          child: Text('Eliminar usuario', style: TextStyle(color: Colors.red)),
+        ),
+        SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => UserListScreen()),
+            );
+          },
+          child: Text('Ver todos los usuarios'),
+        ),
+        SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => UpdateUserScreen()),
+            );
+          },
+          child: Text('Actualizar usuario'),
+        ),
+      ],
+    );
+  }
+}
